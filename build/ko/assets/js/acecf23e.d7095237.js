a9ba0f8f9a04e79a29a232ddacee671295f23ee3
suhyun-forten-lee <nine01223@kakao.com>
2024-11-19 23:20:57 +0900

    docs: test

--- build/ko/assets/js/acecf23e.d7095237.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/architecture/ko/blog","blogTitle":"Blog","authorsListPath":"/architecture/ko/blog/authors"}')}}]);
--- versioned_docs/version-1.000/Server/did-api-server/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# Changelog
-
-## v1.0.0 (2024-10-18)
-
-### 🚀 New Features
-- API Gateway Server
-    - Get DID Document
-    - Get VC Metadata
